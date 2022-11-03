a3d68dc0b114df87e42e88138e8cd81fe2b3afea
Novante <78790665+Novante@users.noreply.github.com>
2022-11-03 17:58:29 +0100

    Added code for checking player names in all player types

--- app.js
+++ app.js
@@ -2,27 +2,54 @@ import puppeteer from "puppeteer/internal/puppeteer.js";
 
 // bot setup
 
-    const browser = await puppeteer.launch({headless: false})
-    const page = await browser.newPage();
+const browser = await puppeteer.launch({headless: false})
+const page = await browser.newPage();
 
-    await page.goto('https://codenames.game/room/piano-shoulder-wagon', {waitUntil: 'networkidle0'})
+await page.goto('https://codenames.game/room/reindeer-russia-stick', {waitUntil: 'networkidle0'})
 
-    await page.type('#nickname-input', 'CodeBot')
-    await page.click('.button.jsx-2506078312')
+await page.type('#nickname-input', 'CodeBot')
+await page.click('.button.jsx-2506078312')
 
 await page.waitForNetworkIdle({idleTime: 2000})
-await page.screenshot({path: 'test.png'})
 
+await page.waitForFunction(
+    'document.querySelector("#layoutRoot").innerText.includes("you need to join")'
+)
 
-// let redPlayerList = await page.evaluate(() => document.querySelector('.jsx-1562487247 > :nth-child(3)').querySelector('section').innerText)
+console.log('set up done')
 
 let redPlayerList = await page.evaluate(() => {
-    let res = Array.from(document.querySelector('.jsx-1562487247 > :nth-child(3)').querySelectorAll('section'))
+    let res = Array.from(document.querySelector('.red > :nth-child(3)').querySelectorAll('section'))
+    let ans = res.map(el => {
+        return el.innerText
+    })
+    return ans
+})
+
+let redSpymasterList = await page.evaluate(() => {
+    let res = Array.from(document.querySelector('.red > :nth-child(6)').querySelectorAll('section'))
+    let ans = res.map(el => {
+        return el.innerText
+    })
+    return ans
+})
+
+let bluePlayerList = await page.evaluate(() => {
+    let res = Array.from(document.querySelector('.blue > :nth-child(3)').querySelectorAll('section'))
     let ans = res.map(el => {return el.innerText})
     return ans
 })
 
-console.log(redPlayerList)
+let blueSpymasterList = await page.evaluate(() => {
+    let res = Array.from(document.querySelector('.blue > :nth-child(6)').querySelectorAll('section'))
+    let ans = res.map(el => {
+        return el.innerText
+    })
+    return ans
+})
+
+
+
 
 
 
